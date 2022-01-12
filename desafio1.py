4fd49441d5be9a2967a446b2d36b5baf45927ea6
Thais Souza <75867936+so-tha@users.noreply.github.com>
2022-01-11 23:14:01 -0300

    Add desafios123

---
+++ desafio1.py
@@ -0,0 +1,3 @@
+nome = input('Qual é o seu nome? ')
+
+print('Seja bem vindo',nome)
---
+++ desafio3.py
@@ -0,0 +1,6 @@
+numberO = int(input('Informe um numero: '))
+numberT = int(input('Informe mais um numero: '))
+
+sums = numberO + numberT
+
+print(sums)
